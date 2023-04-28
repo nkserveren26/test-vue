e8c0c7e9086fce73338334bad38c744c3c7cd55d
nkserveren26 <norinoritoshi@outlook.jp>
2023-04-28 22:38:00 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { computed,getCurrentInstance } from 'vue';
+
+const usevuetify = () => {
+  const instance = getCurrentInstance()
+  if (!instance) {
+    throw new Error(`useVuetify should be called in setup().`)
+  }
+  return instance.proxy.$vuetify
+}
+
+const answer = computed(function()  { 
+  const vuetify = usevuetify()
+  console.log(vuetify)
+  
+  return vuetify.display.xs ? 450 : '250';
+})
+</script>
+
+<template>
+  <v-app>
+    <v-card
+        width="400"
+        :height="answer"
+        title="This is a title"
+        subtitle="This is a subtitle"
+        text="This is content"
+      >
+    </v-card>
+  </v-app>
+</template>
+
+
